bdfc6e4c80de525c79f44b98d918c10c8724a5cf
Gasanova Anastasiya <iamgasanova@gmail.com>
2019-12-21 00:34:45 +0300

    fix test

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -5,8 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestClass {
     WebDriver driver;
@@ -31,10 +38,23 @@ public class TestClass {
     @Test
     void someTest(){
         open("http://localhost:9999");
-        $$("[name='name']").setValue("Петров Василий");
-        $$("[name='phone']").setValue("+79200000000");
+        $$("[class='input__control']").first().setValue("Уфа");
 
-    }
+        Date currentDate = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        c.add(Calendar.DATE, 2);
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        String meetDateTxtValue = (df.format(c.getTime()));
+        $$("[class='input__control']").get(1).setValue(meetDateTxtValue);
+        $$("[class='input__control']").get(2).setValue("Петров Василий");
+        $$("[class='input__control']").get(3).setValue("+79200000000");
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+
+        $(withText("Встреча успешно забронирована")).waitUntil(Condition.visible,15000);
+
+       }
 
 
 }
